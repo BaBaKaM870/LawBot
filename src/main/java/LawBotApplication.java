e3f4da7e3033a7dd1d157b7e3a2c42fd386afc39
BaBaKaM870 <yohan.dechamps131@gmail.com>
2026-03-03 10:03:28 +0100

    Correction bug + ajout de LawBotApllication.java

--- src/main/java/LawBotApplication.java
+++ src/main/java/LawBotApplication.java
@@ -1,7 +1,7 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"controller", "service", "model"})
+@SpringBootApplication(scanBasePackages = {"controller", "service", "model", "game"})
 public class LawBotApplication {
     public static void main(String[] args) {
         SpringApplication.run(LawBotApplication.class, args);
